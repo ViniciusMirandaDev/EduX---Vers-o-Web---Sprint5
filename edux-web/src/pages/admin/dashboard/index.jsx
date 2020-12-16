54e99da2eb107eb01ff44af109fb6914fab7973e
ViniciusMirandaDev <vinimb07@gmail.com>
2020-12-16 17:28:13 -0300

    Mudanças

--- edux-web/src/pages/admin/dashboard/index.jsx
+++ edux-web/src/pages/admin/dashboard/index.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
     return (
         <div>
             <Menu />
-                <h1 style={{marginLeft:"670px", marginTop:"20px"}}> DashBoard </h1>
+                <h1 style={{marginLeft:"665px", marginTop:"20px"}}> DashBoard </h1>
                 <br/>
                 <p style={{marginLeft:"300px"}}>Esta página irá ser lançada futuramente quando houverem mais atualizações no sistema, pois ainda não é possível captar as estatísticas.</p>
                 <p style={{marginLeft:"700px"}}>Atenciosamente,
