4166a988d6c72a1649e7b3c3855d92d9f0337869
Yan Ad'Vincula Trindade <61596255+Yan-91@users.noreply.github.com>
2020-12-17 13:49:16 -0300

    ObjetivoAluno Feito

--- edux-web/src/pages/aluno/objetivos/index.jsx
+++ edux-web/src/pages/aluno/objetivos/index.jsx
@@ -1,17 +1,100 @@
 import React from 'react';
 import Menu from '../../../components/menu';
 import Rodape from '../../../components/rodape';
-
+import {Card, Button, Container, Row, Col} from "react-bootstrap";
 
 const ObjetivosAluno = () => {
 
     
     return (
         <div>
-            <Menu />
-            <h1 style={{marginLeft:"684px", marginTop:"20px"}}> Objetivos </h1>
-            < Rodape/>
-        </div> 
+            <Menu/>
+            
+            <Container>
+            <h2 style={{lineHeight: "300%", textAlign: "center"}}>Objetivos</h2>
+                <Row style={{marginTop: "50px"}}>
+                   
+                    
+                        
+                                <Col  xs={4}>
+                                    <Card style={{margin: "25px"}}>
+                                        <Card.Body>
+                                            <Card.Title>Matemática</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                                            <Card.Text>
+                                                Você tem 2 Objetivos para cumprir
+                                            </Card.Text>
+                                            
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col  xs={4}>
+                                    <Card style={{margin: "25px"}}>
+                                        <Card.Body>
+                                            <Card.Title>Português</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                                            <Card.Text>
+                                                Você tem 1 Objetivos para cumprir
+                                            </Card.Text>
+                                            
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col xs={4}>
+                                    <Card style={{margin: "25px"}}>
+                                        <Card.Body>
+                                            <Card.Title>Biologia</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                                            <Card.Text>
+                                                Você tem 5 Objetivos para cumprir
+                                            </Card.Text>
+                                            
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col  xs={4}>
+                                    <Card style={{margin: "25px"}}>
+                                        <Card.Body>
+                                            <Card.Title>Geografia</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                                            <Card.Text>
+                                                Você tem 8 Objetivos para cumprir
+                                            </Card.Text>
+                                            
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col  xs={4}>
+                                    <Card style={{margin: "25px"}}>
+                                        <Card.Body>
+                                            <Card.Title>Historia</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                                            <Card.Text>
+                                                Você tem 3 Objetivos para cumprir
+                                            </Card.Text>
+                                            
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col  xs={4}>
+                                    <Card style={{margin: "25px"}}>
+                                        <Card.Body>
+                                            <Card.Title>Desenvolvimento</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                                            <Card.Text>
+                                                Você tem 1 Objetivos para cumprir
+                                            </Card.Text>
+                                            
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            
+                        
+                    
+                </Row>
+            </Container>
+            <Rodape/>
+        </div>
     )
 }
 
